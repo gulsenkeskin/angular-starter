038fb01c47852c97dd280ac2a93bd01e135ef20b
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-09-20 23:33:15 +0300

    upd navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AboutComponent } from './modules/general/about/about.component';
-import { ContactComponent } from './modules/general/contact/contact.component';
 import { HomeComponent } from './modules/general/home/home.component';
-import { LoginComponent } from './modules/general/login/login.component';
 import { NotFoundComponent } from './modules/general/not-found/not-found.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
   { path: 'contact', loadChildren: () => import('./modules/general/contact/contact.module').then(mod => mod.ContactModule) },
   { path: 'about', loadChildren: () => import('./modules/general/about/about.module').then(mod => mod.AboutModule) },
   { path: 'login', loadChildren: () => import('./modules/general/login/login.module').then(mod => mod.LoginModule) },
