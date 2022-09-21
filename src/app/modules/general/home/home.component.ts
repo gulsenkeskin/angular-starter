6e0a34958d283edc7bea8255adab84ae7b5dc48f
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-09-21 13:15:54 +0300

    upd navbar

--- src/app/modules/general/home/home.component.ts
+++ src/app/modules/general/home/home.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from '../../../../environments/environment';
+import { Inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +11,84 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+
+  name = environment.application.name;
+  angular = environment.application.angular;
+  bootstrap = environment.application.bootstrap;
+  fontawesome = environment.application.fontawesome;
+
+  features: any;
+  constructor(
+    private meta: Meta,
+    private titleService: Title,
+    @Inject(PLATFORM_ID) private platformId: object) {
+    this.features =
+      [
+        {
+          name: 'Items',
+          description: 'Items',
+          icon: 'fab fa-bootstrap',
+          link: 'httpclient'
+        },
+        {
+          name: 'Bootstrap',
+          description: 'How to use Buttons, Alerts, Pagination, Tables, Collapses',
+          icon: 'fab fa-bootstrap',
+          link: 'bootstrap'
+        },
+        {
+          name: 'Components',
+          description: 'Channel component with Input, Output and Event Emitter',
+          icon: 'far fa-clone',
+          link: 'components'
+        },
+        {
+          name: 'Services',
+          description: 'Use services to view a playlist and a youtube player',
+          icon: 'fas fa-handshake',
+          link: 'services'
+        },
+        {
+          name: 'Reactive Forms',
+          description: 'A model-driven approach to handling form inputs',
+          icon: 'far fa-file-alt',
+          link: 'forms'
+        },
+        {
+          name: 'Template Driven',
+          description: 'Forms are the mainstay of business applications',
+          icon: 'far fa-file-alt',
+          link: 'forms'
+        },
+      ];
+
+  }
 
   ngOnInit(): void {
+
+    if (isPlatformBrowser(this.platformId)) {
+      let navMain = document.getElementById('navbarCollapse');
+      if (navMain) {
+        navMain.onclick = function () {
+          if (navMain) {
+            navMain.classList.remove("show");
+          }
+        }
+      }
+    }
+
+    this.titleService.setTitle('angular.gulsen: Une Application Web avec Angular');
+    this.meta.addTag({
+      name: 'angular.gulsen',
+      content: 'Gülsen'
+    });
+    this.meta.updateTag(
+      {
+        name: 'description',
+        content: 'Cette application a été développée avec Angular version 13.1.1 et bootstrap 5.1.3' +
+          ' Elle applique le Routing, le Lazy loading, le Server side rendering et les Progressive Web App (PWA)'
+      });
+
   }
 
 }
