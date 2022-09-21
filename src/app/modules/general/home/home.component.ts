eb33bfb4d8d506f0e64b2f8b47eb3adf4cab3051
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-09-21 13:24:19 +0300

    upd items component

--- src/app/modules/general/home/home.component.ts
+++ src/app/modules/general/home/home.component.ts
@@ -30,36 +30,7 @@ export class HomeComponent implements OnInit {
           icon: 'fab fa-bootstrap',
           link: 'httpclient'
         },
-        {
-          name: 'Bootstrap',
-          description: 'How to use Buttons, Alerts, Pagination, Tables, Collapses',
-          icon: 'fab fa-bootstrap',
-          link: 'bootstrap'
-        },
-        {
-          name: 'Components',
-          description: 'Channel component with Input, Output and Event Emitter',
-          icon: 'far fa-clone',
-          link: 'components'
-        },
-        {
-          name: 'Services',
-          description: 'Use services to view a playlist and a youtube player',
-          icon: 'fas fa-handshake',
-          link: 'services'
-        },
-        {
-          name: 'Reactive Forms',
-          description: 'A model-driven approach to handling form inputs',
-          icon: 'far fa-file-alt',
-          link: 'forms'
-        },
-        {
-          name: 'Template Driven',
-          description: 'Forms are the mainstay of business applications',
-          icon: 'far fa-file-alt',
-          link: 'forms'
-        },
+
       ];
 
   }
