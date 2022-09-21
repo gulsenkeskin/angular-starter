a89b5e76c394012122bcc1abf65c4de010783d0a
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-09-21 10:28:06 +0300

    added ItemsService in items module

--- src/app/modules/application/items/items.module.ts
+++ src/app/modules/application/items/items.module.ts
@@ -3,15 +3,18 @@ import { CommonModule } from '@angular/common';
 
 import { ItemsRoutingModule } from './items-routing.module';
 import { ItemsComponent } from './items.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ItemsService } from './items.service';
 
 
 @NgModule({
   declarations: [ItemsComponent],
   imports: [
     CommonModule,
-    ItemsRoutingModule
+    ItemsRoutingModule,
+    HttpClientModule
   ],
   exports: [ItemsComponent],
-  providers: []
+  providers: [ItemsService]
 })
 export class ItemsModule { }
